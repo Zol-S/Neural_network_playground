04f09d722ff43c0e92b545331c9eb6cf0d480a7d
Zoltán Balogh <zoltan.balogh.jr@gmail.com>
2020-11-30 21:59:15 +0100

    Description is now fixed
    Description is now fixed

--- public/js/backbone/views/cnn_activation_maximizer.js
+++ public/js/backbone/views/cnn_activation_maximizer.js
@@ -13,7 +13,7 @@ define([
 		models: {
 			'mnist_28x28': {
 				title: 'MNIST 28x28',
-				description: 'Model trained on MNIST digit recognition dataset, it is not suitable for face recognition. Note that it was trained on black and white input, that\'s why it gives a better prediction for the black and white input.'
+				description: 'The model of MNIST database of handwritten digits. It has a training set of 60,000 examples, and a test set of 10,000 examples. The digits have been size-normalized and centered in a fixed-size image.'
 			}
 		},
 		initialize: function() {

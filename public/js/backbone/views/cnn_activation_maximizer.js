ba13883d7196d9e03b6e6cc16b28fbbb0919567b
Zoltán Balogh <zbalogh@hearsaycorp.com>
2024-12-01 12:17:28 +0100

    Update cnn_activation_maximizer.js

--- public/js/backbone/views/cnn_activation_maximizer.js
+++ public/js/backbone/views/cnn_activation_maximizer.js
@@ -3,7 +3,8 @@ define([
 	'underscore',
 	'backbonejs',
 	'text!backbone/templates/cnn_activation_maximizer.html',
-	'tfjs174',
+	//'tfjs174',
+	'tfjs4220',
 	'bootstrap'
 ], function ($, _, Backbone, CNNActivationMaximizerTemplate, tf) {
 	'use strict';
